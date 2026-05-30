5490ecf38341aef1e5db83bffb0db628b1df656f
stackfiendllc-debug <stackfiendllc@gmail.com>
2026-05-29 23:27:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-.
+let mining = false;
+let earned = 0;
+let hashPower = 0;
+
+document.getElementById("connectWallet").addEventListener("click", () => {
+  alert("Phantom Wallet Connected");
+});
+
+document.getElementById("mineButton").addEventListener("click", () => {
+  mining = !mining;
+
+  if (mining) {
+    document.getElementById("mineButton").innerText = "MINING...";
+    startMining();
+  } else {
+    document.getElementById("mineButton").innerText = "START MINING";
+  }
+});
+
+function startMining() {
+  const interval = setInterval(() => {
+    if (!mining) {
+      clearInterval(interval);
+      return;
+    }
+
+    earned += 5;
+    hashPower = Math.floor(Math.random() * 500 + 100);
+
+    document.getElementById("earned").innerText = earned + " VLX";
+    document.getElementById("balance").innerText = earned + " VLX";
+    document.getElementById("hashPower").innerText = hashPower + " H/s";
+  }, 1000);
+}
